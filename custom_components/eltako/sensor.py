b45c769917e442c9ecbc632f4aa85b89406dd43f
el-mojito <jopeter@gmx.de>
2025-08-04 22:12:56 +0200

    Cleanup

--- custom_components/eltako/sensor.py
+++ custom_components/eltako/sensor.py
@@ -48,7 +48,6 @@ from .const import *
 from . import get_gateway_from_hass, get_device_config_for_gateway
 
 DEFAULT_DEVICE_NAME_WINDOW_HANDLE = "Window handle"
-DEFAULT_DEVICE_NAME_WINDOW_HANDLE_EXT = "Window handle ext"
 DEFAULT_DEVICE_NAME_WEATHER_STATION = "Weather station"
 DEFAULT_DEVICE_NAME_ELECTRICITY_METER = "Electricity meter"
 DEFAULT_DEVICE_NAME_GAS_METER = "Gas meter"
@@ -70,7 +69,6 @@ SENSOR_TYPE_HUMIDITY = "humidity"
 SENSOR_TYPE_VOLTAGE = "voltage"
 SENSOR_TYPE_PIR = "pir"
 SENSOR_TYPE_WINDOWHANDLE = "windowhandle"
-SENSOR_TYPE_WINDOWHANDLE_EXT = "windowhandle_ext"
 SENSOR_TYPE_WEATHER_STATION_ILLUMINANCE_DAWN = "weather_station_illuminance_dawn"
 SENSOR_TYPE_WEATHER_STATION_TEMPERATURE = "weather_station_temperature"
 SENSOR_TYPE_WEATHER_STATION_WIND_SPEED = "weather_station_wind_speed"
@@ -149,17 +147,6 @@ SENSOR_DESC_WATER_CURRENT = EltakoSensorEntityDescription(
 SENSOR_DESC_WINDOWHANDLE = EltakoSensorEntityDescription(
     key=SENSOR_TYPE_WINDOWHANDLE,
     name="Window handle",
-    icon="mdi:window-open-variant",
-    device_class='window',
-    native_unit_of_measurement=None,
-    suggested_display_precision=None,
-    suggested_unit_of_measurement=None,
-    state_class=None
-)
-
-SENSOR_DESC_WINDOWHANDLE_EXT = EltakoSensorEntityDescription(
-    key=SENSOR_TYPE_WINDOWHANDLE_EXT,
-    name="Window handle ext",
     icon="mdi:window-open-variant",
     device_class='window',
     native_unit_of_measurement=None,
@@ -335,9 +322,10 @@ async def async_setup_entry(
                     
                 elif dev_conf.eep in [A5_14_09]:
                     if dev_name == "":
-                        dev_name = DEFAULT_DEVICE_NAME_WINDOW_HANDLE_EXT
+                        dev_name = DEFAULT_DEVICE_NAME_WINDOW_HANDLE
                     
-                    entities.append(EltakoWindowHandleExt(platform, gateway, dev_conf.id, dev_name, dev_conf.eep, SENSOR_DESC_WINDOWHANDLE_EXT))
+                    entities.append(EltakoWindowHandleWithSupplyVoltage(platform, gateway, dev_conf.id, dev_name, dev_conf.eep, SENSOR_DESC_WINDOWHANDLE))
+                    entities.append(EltakoBatteryVoltageSensor(platform, gateway, dev_conf.id, dev_name, dev_conf.eep))
                     
                 elif dev_conf.eep in [A5_12_01]:
                     if dev_name == "":
@@ -630,7 +618,7 @@ class EltakoWindowHandle(EltakoSensor):
 
         self.schedule_update_ha_state()
 
-class EltakoWindowHandleExt(EltakoSensor):
+class EltakoWindowHandleWithSupplyVoltage(EltakoSensor):
     """Representation of an Eltako window handle device.
 
     EEPs (EnOcean Equipment Profiles):
@@ -649,11 +637,11 @@ class EltakoWindowHandleExt(EltakoSensor):
             LOGGER.warning("[Window Handle Sensor %s] Could not decode message: %s", self.dev_id, str(e))
             return
         
-        if decoded.handle_position == WindowHandlePosition.CLOSED:
+        if decoded.handle_position == WindowHandleWithSupplyVoltagePosition.CLOSED:
             self._attr_native_value = STATE_CLOSED
-        elif decoded.handle_position == WindowHandlePosition.OPEN:
+        elif decoded.handle_position == WindowHandleWithSupplyVoltagePosition.OPEN:
             self._attr_native_value = STATE_OPEN
-        elif decoded.handle_position == WindowHandlePosition.TILT:
+        elif decoded.handle_position == WindowHandleWithSupplyVoltagePosition.TILT:
             self._attr_native_value = "tilt"
         else:
             return
