7c6a83e08ad5828f7cfe5fe36294166ad9757c34
Johannes Peter <jopeter@gmx.de>
2025-08-16 00:57:58 +0200

    Fix test for EEP A5-14-09

--- tests/test_sensor_A5_14_09.py
+++ tests/test_sensor_A5_14_09.py
@@ -13,8 +13,6 @@ Entity.schedule_update_ha_state = mock.Mock(return_value=None)
 
 
 class TestSensor_A5_14_09(unittest.TestCase):
-    
-    msg1 = Regular4BSMessage(address=b'\xFF\xFF\x00\x80', data=b'\xAA\x00\x00\xD0', status=0x00)
 
     def create_windowhandle_sensor(self) -> EltakoWindowHandleWithSupplyVoltage:
         gateway = GatewayMock()
@@ -37,17 +35,27 @@ class TestSensor_A5_14_09(unittest.TestCase):
 
     def test_windowhandle_sensor(self):
         ewh_handle = self.create_windowhandle_sensor()
+
+        self.assertEqual(ewh_handle.native_value, None)
         
-        ewh_handle.entity_description = SENSOR_DESC_WINDOWHANDLE
-        ewh_handle._attr_native_value = -1
+        msg = Regular4BSMessage(address=b'\xFF\xFF\x00\x80', data=b'\xAA\x00\x00\x08', status=0x00)
+        ewh_handle.value_changed(msg)
+        self.assertEqual(ewh_handle.native_value, STATE_CLOSED)
         
-        ewh_handle.value_changed(self.msg1)
+        msg = Regular4BSMessage(address=b'\xFF\xFF\x00\x80', data=b'\xAA\x00\x00\x0E', status=0x00)
+        ewh_handle.value_changed(msg)
+        self.assertEqual(ewh_handle.native_value, STATE_OPEN)
+        
+        msg = Regular4BSMessage(address=b'\xFF\xFF\x00\x80', data=b'\xAA\x00\x00\x0A', status=0x00)
+        ewh_handle.value_changed(msg)
         self.assertEqual(ewh_handle.native_value, 'tilt')
 
         
     def test_battery_voltage_sensor(self):
+        msg = Regular4BSMessage(address=b'\xFF\xFF\x00\x80', data=b'\xAA\x00\x00\x0A', status=0x00)
+
         ebs_vlt = self.create_battery_voltage_sensor()
 
-        ebs_vlt.value_changed(self.msg1)
-        self.assertEqual(ebs_vlt.native_value, 3.3999999999999995)
+        ebs_vlt.value_changed(msg)
+        self.assertAlmostEqual(ebs_vlt.native_value, 3.4, delta=1e-6)
 
